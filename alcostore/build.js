ab72a06f607ffd4963ae36c13689b06d0d9cb76e
fire567 <dante.nero.95@gmail.com>
2019-02-22 21:06:25 +0300

    build v2

--- alcostore/build.js
+++ alcostore/build.js
@@ -6,23 +6,28 @@ const wrapTemplate = (name, content)=>{
   return `<script id="${name}Tmpl" type="text/x-jquery-tmpl">${content}</script>`
 };
 
-let content = '';
-fs.readFile('./src/index.html', 'utf8', (err, index)=>  {
-  content += index;
-  fs.readFile('./src/pages/home.html', 'utf8', (err, home)=> {
-    content += wrapTemplate('home', home);
-    fs.readFile('./src/pages/glintwain.html', 'utf8', (err, glintwain)=> {
-      content += wrapTemplate('glintwain', glintwain);
-      fs.readFile('./src/pages/vermut.html', 'utf8', (err, vermut)=> {
-        content += wrapTemplate('vermut', vermut);
-        fs.writeFile('dist/index.html', content, ()=>{});
-      })
-    })
-  })
+const defaultCallback = ()=> {};
+const ENCODING = 'utf8';
+const PATH = {
+  _wrap(folder, relPath) {
+    let path = `./${folder}`;
+    if (relPath) path += `/${relPath}`;
+    return path;
+  },
+  src(relPath) { return this._wrap('src', relPath) },
+  dist(relPath) { return this._wrap('dist', relPath) },
+  index() { return `${this.src()}/index.html` },
+  page(name) { return `${this.src()}/pages/${name}` }
+};
+
+let htmlContent = fs.readFileSync(PATH.index(), ENCODING);
+fs.readdir(PATH.src('pages'), function (err, files) {
+  files.forEach(function (fileName, index) {
+    const pageName = fileName.split('.')[0];
+    const pageContent = fs.readFileSync(PATH.page(fileName), ENCODING);
+    htmlContent += wrapTemplate(pageName, pageContent);
+  });
+  fs.writeFile(PATH.dist('index.html'), htmlContent, defaultCallback);
 });
-
-
-
-// <script id="glintwainTmpl" type="text/x-jquery-tmpl">
-//
-//   </script>
+fs.copyFile(PATH.src('stylesheets.css'), PATH.dist('stylesheets.css'), defaultCallback);
+fs.copyFile(PATH.src('scripts.js'), PATH.dist('scripts.js'), defaultCallback);
