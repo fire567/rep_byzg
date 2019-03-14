73f8f3e16e7c454fe947bccbe7eafdba42f66978
fire567 <dante.nero.95@gmail.com>
2019-03-14 21:32:08 +0300

    babel-preset-react

---
+++ alcostore/src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="some-css-class">
+        тут находится содержимое элемента с css-классом "some-css-class"
+      </div>
+    )
+  }
+}
+
