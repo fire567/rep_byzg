db3ec546893c3f19cf59e0149a44f95a178d497c
fire567 <dante.nero.95@gmail.com>
2019-03-25 21:14:20 +0300

    global style

--- alcostore/src/index.js
+++ alcostore/src/index.js
@@ -2,17 +2,21 @@ import React  from 'react';
 import { Router, Route, browserHistory } from 'react-router'
 import ReactDOM from 'react-dom';
 
+import { GlobalStyle } from './style.global';
 import App from './components/App/App';
 import HomePage from './pages/Home/Home';
 import GlintwainPage from './pages/Glintwain/Glintwain';
 
 ReactDOM.render((
-  <Router history={browserHistory} >
-    <Route component={App} >
-      <Route path="/" component={HomePage}  />
-      <Route path="glintwain" component={GlintwainPage}  />
-    </Route>
-  </Router>
+  <div>
+    <GlobalStyle />
+    <Router history={browserHistory} >
+      <Route component={App} >
+        <Route path="/" component={HomePage}  />
+        <Route path="glintwain" component={GlintwainPage}  />
+      </Route>
+    </Router>
+  </div>
   ),
   document.getElementById('root')
 );
