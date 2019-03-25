1fd6fb3e078530ff55c398f763633d5475c3837a
fire567 <dante.nero.95@gmail.com>
2019-03-25 20:51:56 +0300

    remove css-loader and style-loader

--- alcostore/webpack.config.js
+++ alcostore/webpack.config.js
@@ -13,10 +13,6 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
       }
     ]
   },
