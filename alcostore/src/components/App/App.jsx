f9ed6e35e3cdaef637a455b40d376a708a5ca81e
fire567 <dante.nero.95@gmail.com>
2019-03-25 22:05:31 +0300

    eslint config

--- alcostore/src/components/App/App.jsx
+++ alcostore/src/components/App/App.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
-import { Wrapper, Body, Content } from './styled';
+import {Wrapper, Body, Content } from './styled';
 
 export default class App extends Component {
   render() {
